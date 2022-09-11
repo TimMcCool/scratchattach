c18ba712684d3203798f771cc6e0b7e1fcc95997
Tim <53166177+TimMcCool@users.noreply.github.com>
2022-09-11 21:22:16 +0200

    Formatted using black.vercel.app
    Signed-off-by: Tim <53166177+TimMcCool@users.noreply.github.com>

--- scratchattach/_cloud.py
+++ scratchattach/_cloud.py
@@ -1,4 +1,4 @@
-#----- Cloud interactions
+# ----- Cloud interactions
 from numpy import var
 import websocket
 import json
@@ -7,20 +7,31 @@ from threading import Thread
 import time
 from . import _exceptions
 
-class _CloudMixin:
 
-    def __init__(self, *, project_id, username="python", session_id=None, cloud_host=None, _allow_non_numeric=False, _no_reconnect=False):
+class _CloudMixin:
+    def __init__(
+        self,
+        *,
+        project_id,
+        username="python",
+        session_id=None,
+        cloud_host=None,
+        _allow_non_numeric=False,
+        _no_reconnect=False,
+    ):
         self._session_id = session_id
         self._username = username
         self.project_id = str(project_id)
-        self._ratelimited_until = 0#deals with the 0.1 second rate limit for cloud variable sets
+        self._ratelimited_until = (
+            0  # deals with the 0.1 second rate limit for cloud variable sets
+        )
         self.websocket = websocket.WebSocket()
         self._no_reconnect = False
         self._connect(cloud_host=cloud_host)
         self._handshake()
         self._no_reconnect = _no_reconnect
         self.cloud_host = cloud_host
-        self.allow_non_numeric = _allow_non_numeric #TurboWarp only. If this is true, turbowarp cloud variables can be set to non-numeric values (if) the cloud_host wss allows it)
+        self.allow_non_numeric = _allow_non_numeric  # TurboWarp only. If this is true, turbowarp cloud variables can be set to non-numeric values (if) the cloud_host wss allows it)
 
     def _send_packet(self, packet):
         self.websocket.send(json.dumps(packet) + "\n")
@@ -33,7 +44,11 @@ class _CloudMixin:
             return
         try:
             self._send_packet(
-                {"method": "handshake", "user": self._username, "project_id": self.project_id}
+                {
+                    "method": "handshake",
+                    "user": self._username,
+                    "project_id": self.project_id,
+                }
             )
         except Exception:
             self._connect(cloud_host=self.cloud_host)
@@ -42,25 +57,26 @@ class _CloudMixin:
     def get_var(self, variable):
         try:
             variable = "☁ " + str(variable)
-            self.set_var('@scratchattach','0')
+            self.set_var("@scratchattach", "0")
 
             result = []
             for i in self._clouddata:
                 try:
                     result.append(json.loads(i))
-                except Exception: pass
+                except Exception:
+                    pass
             if result == []:
                 return None
             else:
                 for i in result:
-                    if i['name'] == variable:
-                        return i['value']
+                    if i["name"] == variable:
+                        return i["value"]
                 return None
         except Exception as e:
             raise _exceptions.FetchError
 
-class CloudConnection(_CloudMixin):
 
+class CloudConnection(_CloudMixin):
     def _connect(self, *, cloud_host):
         try:
             self.websocket = websocket.WebSocket()
@@ -71,19 +87,19 @@ class CloudConnection(_CloudMixin):
                 enable_multithread=True,
             )
         except Exception:
-            raise(_exceptions.ConnectionError)
+            raise (_exceptions.ConnectionError)
 
     def set_var(self, variable, value):
         variable = variable.replace("☁ ", "")
         value = str(value)
         if len(value) > 256:
             print("invalid cloud var (too long):", value)
-            raise(_exceptions.InvalidCloudValue)
+            raise (_exceptions.InvalidCloudValue)
         x = value.replace(".", "")
         x = x.replace("-", "")
         if not x.isnumeric():
             print("invalid cloud var (not numeric):", value)
-            raise(_exceptions.InvalidCloudValue)
+            raise (_exceptions.InvalidCloudValue)
         while self._ratelimited_until + 0.1 >= time.time():
             pass
         try:
@@ -97,7 +113,7 @@ class CloudConnection(_CloudMixin):
                 }
             )
             if variable == "@scratchattach":
-                self._clouddata = self.websocket.recv().split('\n')
+                self._clouddata = self.websocket.recv().split("\n")
         except Exception as e:
             try:
                 self._handshake()
@@ -109,8 +125,8 @@ class CloudConnection(_CloudMixin):
             self.set_var(variable, value)
         self._ratelimited_until = time.time()
 
-class TwCloudConnection(_CloudMixin):
 
+class TwCloudConnection(_CloudMixin):
     def _connect(self, *, cloud_host):
         if self._no_reconnect is True:
             return
@@ -118,23 +134,21 @@ class TwCloudConnection(_CloudMixin):
             if cloud_host is None:
                 cloud_host = "wss://clouddata.turbowarp.org/"
                 self.cloud_host = "wss://clouddata.turbowarp.org/"
-            self.websocket.connect(
-                cloud_host,
-                enable_multithread=True
-            )
+            self.websocket.connect(cloud_host, enable_multithread=True)
         except Exception:
-            raise(_exceptions.ConnectionError)
+            raise (_exceptions.ConnectionError)
 
     def get_cloud(self, variable):
         try:
             variable = "☁ " + str(variable)
-            self.set_var('@scratchattach','0')
+            self.set_var("@scratchattach", "0")
 
             result = []
             for i in self._clouddata:
                 try:
                     result.append(json.loads(i))
-                except Exception: pass
+                except Exception:
+                    pass
             data = {}
             if result == []:
                 return {}
@@ -145,7 +159,6 @@ class TwCloudConnection(_CloudMixin):
         except Exception as e:
             raise _exceptions.FetchError
 
-
     def set_var(self, variable, value):
         variable = variable.replace("☁ ", "")
         value = str(value)
@@ -153,7 +166,7 @@ class TwCloudConnection(_CloudMixin):
         x = x.replace("-", "")
         if not x.isnumeric():
             if self.allow_non_numeric is False:
-                raise(_exceptions.InvalidCloudValue)
+                raise (_exceptions.InvalidCloudValue)
 
         while self._ratelimited_until + 0.1 > time.time():
             pass
@@ -168,7 +181,7 @@ class TwCloudConnection(_CloudMixin):
                 }
             )
             if variable == "@scratchattach":
-                self._clouddata = self.websocket.recv().split('\n')
+                self._clouddata = self.websocket.recv().split("\n")
         except Exception as e:
             try:
                 self._handshake()
@@ -184,19 +197,18 @@ class TwCloudConnection(_CloudMixin):
 
 
 class CloudEvents:
-
     class Event:
         def __init__(self, **entries):
             self.__dict__.update(entries)
 
     def __init__(self, project_id):
         self.project_id = int(project_id)
-        self.data = get_cloud_logs(project_id=self.project_id, limit = 15)
+        self.data = get_cloud_logs(project_id=self.project_id, limit=15)
         self._thread = None
         self.running = False
         self._events = {}
 
-    def start(self, *, update_interval = 0.1, thread=True, daemon=False):
+    def start(self, *, update_interval=0.1, thread=True, daemon=False):
         if self.running is False:
             self.update_interval = update_interval
             self.running = True
@@ -213,13 +225,21 @@ class CloudEvents:
     def _update(self):
         while True:
             if self.running:
-                data = get_cloud_logs(project_id = self.project_id, limit = 15)
+                data = get_cloud_logs(project_id=self.project_id, limit=15)
                 if data != self.data:
                     for activity in data:
                         if activity in self.data:
                             break
-                        if "on_"+activity["verb"][:-4] in self._events:
-                            self._events["on_"+activity["verb"][:-4]](self.Event(user=activity["user"], var=activity["name"][2:], name=activity["name"][2:], value=activity["value"], timestamp=activity["timestamp"]))
+                        if "on_" + activity["verb"][:-4] in self._events:
+                            self._events["on_" + activity["verb"][:-4]](
+                                self.Event(
+                                    user=activity["user"],
+                                    var=activity["name"][2:],
+                                    name=activity["name"][2:],
+                                    value=activity["value"],
+                                    timestamp=activity["timestamp"],
+                                )
+                            )
                 self.data = data
             else:
                 return
@@ -235,8 +255,8 @@ class CloudEvents:
     def event(self, function):
         self._events[function.__name__] = function
 
-class TwCloudEvents(CloudEvents):
 
+class TwCloudEvents(CloudEvents):
     def __init__(self, project_id):
         cloud_connection = TwCloudConnection(project_id=project_id)
         self.data = []
@@ -247,18 +267,27 @@ class TwCloudEvents(CloudEvents):
 
     def _update(self):
         while True:
-            data = self.connection.websocket.recv().split('\n')
+            data = self.connection.websocket.recv().split("\n")
             result = []
             for i in data:
                 try:
                     result.append(json.loads(i))
-                except Exception: pass
+                except Exception:
+                    pass
             for activity in result:
-                if "on_"+activity["method"] in self._events:
-                    self._events["on_"+activity["method"]](self.Event(user=None, var=activity["name"][2:], name=activity["name"][2:], value=activity["value"], timestamp=time.time()*10000))
+                if "on_" + activity["method"] in self._events:
+                    self._events["on_" + activity["method"]](
+                        self.Event(
+                            user=None,
+                            var=activity["name"][2:],
+                            name=activity["name"][2:],
+                            value=activity["value"],
+                            timestamp=time.time() * 10000,
+                        )
+                    )
+
 
 class WsCloudEvents(CloudEvents):
-
     def __init__(self, project_id, connection):
         self.data = []
         self._thread = None
@@ -269,7 +298,7 @@ class WsCloudEvents(CloudEvents):
     def _update(self):
         while True:
             try:
-                data = self.connection.websocket.recv().split('\n')
+                data = self.connection.websocket.recv().split("\n")
                 result = []
                 for i in data:
                     try:
@@ -277,16 +306,31 @@ class WsCloudEvents(CloudEvents):
                     except Exception:
                         pass
                 for activity in result:
-                    if "on_"+activity["method"] in self._events:
-                        self._events["on_"+activity["method"]](self.Event(user=None, var=activity["name"][2:], name=activity["name"][2:], value=activity["value"], timestamp=time.time()*10000))
+                    if "on_" + activity["method"] in self._events:
+                        self._events["on_" + activity["method"]](
+                            self.Event(
+                                user=None,
+                                var=activity["name"][2:],
+                                name=activity["name"][2:],
+                                value=activity["value"],
+                                timestamp=time.time() * 10000,
+                            )
+                        )
             except Exception:
                 self.connection._connect(cloud_host=self.connection.cloud_host)
                 self.connection._handshake()
+
+
 # -----
+
 
 def get_cloud(project_id):
     try:
-        response = json.loads(requests.get(f"https://clouddata.scratch.mit.edu/logs?projectid={project_id}&limit=100&offset=0").text)
+        response = json.loads(
+            requests.get(
+                f"https://clouddata.scratch.mit.edu/logs?projectid={project_id}&limit=100&offset=0"
+            ).text
+        )
         response.reverse()
         clouddata = {}
         for activity in response:
@@ -295,10 +339,15 @@ def get_cloud(project_id):
     except Exception:
         return []
 
+
 def get_var(project_id, variable):
     try:
-        response = json.loads(requests.get(f"https://clouddata.scratch.mit.edu/logs?projectid={project_id}&limit=100&offset=0").text)
-        response = list(filter(lambda k: k["name"] == "☁ "+variable, response))
+        response = json.loads(
+            requests.get(
+                f"https://clouddata.scratch.mit.edu/logs?projectid={project_id}&limit=100&offset=0"
+            ).text
+        )
+        response = list(filter(lambda k: k["name"] == "☁ " + variable, response))
         if response == []:
             return None
         else:
@@ -306,11 +355,26 @@ def get_var(project_id, variable):
     except Exception:
         return None
 
-def get_cloud_logs(project_id, *, filter_by_var_named =None, limit=25, offset=0, log_url="https://clouddata.scratch.mit.edu/logs"):
+
+def get_cloud_logs(
+    project_id,
+    *,
+    filter_by_var_named=None,
+    limit=25,
+    offset=0,
+    log_url="https://clouddata.scratch.mit.edu/logs",
+):
     try:
-        response = json.loads(requests.get(f"{log_url}?projectid={project_id}&limit={limit}&offset={offset}").text)
-        if filter_by_var_named is None: return response
+        response = json.loads(
+            requests.get(
+                f"{log_url}?projectid={project_id}&limit={limit}&offset={offset}"
+            ).text
+        )
+        if filter_by_var_named is None:
+            return response
         else:
-            return list(filter(lambda k: k["name"] == "☁ "+filter_by_var_named, response))
+            return list(
+                filter(lambda k: k["name"] == "☁ " + filter_by_var_named, response)
+            )
     except Exception:
         return []
