35de30248bb4a76080bce1cf89b3194df12f745a
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-11-16 19:02:54 -0500

    add if name main guard
    Signed-off-by: ajskateboarder <81255936+themysticsavages@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -7,24 +7,25 @@ DESCRIPTION = 'An Scratch API Wrapper for scratch.mit.edu'
 LONG_DESCRIPTION = DESCRIPTION
 
 # Setting up
-setup(
-    name="scratchattach",
-    version=VERSION,
-    author="TimMcCool",
-    author_email="",
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=open('README.md').read(),
-    packages=find_packages(),
-    install_requires=["websocket-client","numpy","requests"],
-    keywords=['scratch api', 'scratchattach', 'scratch api python', 'scratch python', 'scratch for python', 'scratch', 'scratch cloud', 'scratch cloud variables', 'scratch bot'],
-    url='https://github.com/TimMcCool/scratchattach',
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
-)
+if __name__ == "__main__":
+    setup(
+        name="scratchattach",
+        version=VERSION,
+        author="TimMcCool",
+        author_email="",
+        description=DESCRIPTION,
+        long_description_content_type="text/markdown",
+        long_description=open('README.md').read(),
+        packages=find_packages(),
+        install_requires=["websocket-client","numpy","requests"],
+        keywords=['scratch api', 'scratchattach', 'scratch api python', 'scratch python', 'scratch for python', 'scratch', 'scratch cloud', 'scratch cloud variables', 'scratch bot'],
+        url='https://github.com/TimMcCool/scratchattach',
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Unix",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+    )
