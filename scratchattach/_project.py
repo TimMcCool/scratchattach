ef7af7f306fa76d7e08b78bad20e61ec6d25bbd3
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2022-11-16 19:18:06 -0500

    action test
    Signed-off-by: ajskateboarder <81255936+themysticsavages@users.noreply.github.com>

--- scratchattach/_project.py
+++ scratchattach/_project.py
@@ -10,7 +10,7 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
     "x-csrftoken": "a",
     "x-requested-with": "XMLHttpRequest",
-    "referer": "https://scratch.mit.edu",
+    "referer": "https://scratch.mit.edu"
 }
 
 class PartialProject:
