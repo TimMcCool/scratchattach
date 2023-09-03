32a5a2bd761c6076872a748d10fdc67d27913ca6
Tim <53166177+TimMcCool@users.noreply.github.com>
2023-09-03 22:25:58 +0200

    Update setup.py
    Signed-off-by: Tim <53166177+TimMcCool@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '1.1.3'
+VERSION = '1.4.0'
 DESCRIPTION = 'An Scratch API Wrapper for scratch.mit.edu'
 LONG_DESCRIPTION = DESCRIPTION
 
