96ecca6a1cb84ffa76bd52650ea67650a1855096
TimMcCool <timkrome2006@gmail.com>
2024-04-20 03:31:11 +0200

    reduced amount of connections opened with turbowarp

--- scratchattach/cloud_requests.py
+++ scratchattach/cloud_requests.py
@@ -238,7 +238,9 @@ class CloudRequests:
         Sends back the request response to the Scratch project
         """
 
-        if self.idle_since + 8 < time.time() or self.force_reconnect:
+        if (self.idle_since + 8 < time.time() and not isinstance(self.connection, cloud.TwCloudConnection)
+            ) or self.force_reconnect:
+            self.connection.close()
             self.connection._connect(cloud_host=self.connection.cloud_host)
             self.connection._handshake()
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '1.6.8'
+VERSION = '1.6.9'
 DESCRIPTION = 'An Scratch API Wrapper for scra tch.mit.edu'
 LONG_DESCRIPTION = DESCRIPTION
 
