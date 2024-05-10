7a0121f89eb4a3fe34f60ca5d5011dd60d58a2ec
namtacs <95915765+namtacs@users.noreply.github.com>
2024-05-11 00:55:32 +0300

    project.py unlimited rework and reformat
    and add commons.py
    
    Signed-off-by: namtacs <95915765+namtacs@users.noreply.github.com>

---
+++ scratchattach/commons.py
@@ -0,0 +1,32 @@
+"""Common functions used by various internal modules"""
+
+import requests
+
+
+def api_iterative_data(fetch_func, limit, offset, max_req_limit=40, unpack=True):
+    """
+    Iteratively gets data by calling fetch_func with a moving offset and a limit.
+    Once fetch_func returns None, the retrieval is completed.
+    """
+    if limit is None:
+        limit = max_req_limit
+    assert offset >= 0
+    assert limit >= 0
+    end = offset + limit
+    api_data = []
+    for offs in range(offset, end, max_req_limit):
+        # print(offs, max_req_limit)
+        d = fetch_func(
+            offs, max_req_limit
+        )  # Mimick actual scratch by only requesting the max amount
+        if d is None:
+            break
+        if unpack:
+            api_data.extend(d)
+        else:
+            api_data.append(d)
+        if len(d) < max_req_limit:
+            break
+    api_data = api_data[:limit]
+    assert len(api_data) <= limit
+    return api_data
