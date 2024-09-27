f98e5d40d81701e8bdb3fb11f12b5790b701cd3b
Tim <53166177+TimMcCool@users.noreply.github.com>
2024-09-27 04:25:48 +0200

    .

--- scratchattach/__init__.py
+++ scratchattach/__init__.py
@@ -14,40 +14,30 @@ from .eventhandlers.message_events import MessageEvents
 
 def get_news(*, limit=10, offset=0):
     return commons.api_iterative("https://api.scratch.mit.edu/news", limit = limit, offset = offset)
-            
+
+def featured_data():
+    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()
+
 def featured_projects():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["community_featured_projects"]
+    return featured_data()["community_featured_projects"]
 
 def featured_studios():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["community_featured_studios"]
+    return featured_data()["community_featured_studios"]
 
 def top_loved():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["community_most_loved_projects"]
+    return featured_data()["community_most_loved_projects"]
 
 def top_remixed():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["community_most_remixed_projects"]
+    return featured_data()["community_most_remixed_projects"]
 
 def newest_projects():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["community_newest_projects"]
+    return featured_data()["community_newest_projects"]
 
 def curated_projects():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["curator_top_projects"]
+    return featured_data()["curator_top_projects"]
 
 def design_studio_projects():
-    return requests.get("https://api.scratch.mit.edu/proxy/featured").json()["scratch_design_studio"]
-
-def search_posts(*, query, order="newest", page=0):
-    print("Warning: This method is deprecated because ScratchDB is down indefinitely.")
-    try:
-        data = requests.get(f"https://scratchdb.lefty.one/v3/forum/search?q={query}&o={order}&page={page}").json()["posts"]
-        return_data = []
-        for o in data:
-            a = forum.ForumPost(id = o["id"])
-            a._update_from_dict(o)
-            return_data.append(a)
-        return return_data
-    except Exception:
-        return []
+    return featured_data()["scratch_design_studio"]
 
 def total_site_stats():
     data = requests.get("https://scratch.mit.edu/statistics/data/daily/").json()
