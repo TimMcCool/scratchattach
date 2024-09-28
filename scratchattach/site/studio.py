244309e918a732d91b47ae627744ae66133e2bad
Tim <53166177+TimMcCool@users.noreply.github.com>
2024-09-28 03:51:02 +0200

    .

--- scratchattach/site/studio.py
+++ scratchattach/site/studio.py
@@ -187,7 +187,6 @@ class Studio(BaseSiteComponent):
             data=json.dumps(data),
             timeout=10,
         ).json()
-        print(r)
         _comment = comment.Comment(id=r["id"], _session=self._session, source="studio", source_id=self.id)
         _comment._update_from_dict(r)
         return _comment
