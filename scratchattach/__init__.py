63544bc1873ec8e5c21fb5c4b081ae118b51f882
Tim <53166177+TimMcCool@users.noreply.github.com>
2024-10-02 02:46:01 +0200

    cleaned up __init__.py file

--- scratchattach/__init__.py
+++ scratchattach/__init__.py
@@ -1,14 +1,19 @@
-from .cloud.cloud import *
-from .site.user import *
-from .site.session import *
-from .site.project import *
-from .site.studio import *
-#from .eventhandlers.cloud_requests import *
-from .site.forum import *
-from .utils.encoder import *
-from .utils import commons
-from .site.comment import *
-from .eventhandlers.message_events import MessageEvents
-from .eventhandlers.filterbot import Filterbot, HardFilter, SoftFilter, SpamFilter
-from .eventhandlers.cloud_server import *
+from .cloud._base import BaseEventHandler
+from .cloud.cloud import ScratchCloud, TwCloud, get_cloud, get_scratch_cloud, get_tw_cloud
+
+from .eventhandlers._base import BaseEventHandler
+from .eventhandlers.cloud_server import init_cloud_server
+
 from .other.other_apis import *
+
+from .site._base import BaseSiteComponent
+from .site.activity import Activity
+from .site.backpack_asset import BackpackAsset
+from .site.comment import Comment
+from .site.cloud_activity import CloudActivity
+from .site.forum import ForumPost, ForumTopic, get_topic, get_topic_list
+from .site.project import Project, get_project, search_projects, explore_projects
+from .site.session import Session, login, login_by_id, login_by_session_string
+from .site.project import Studio, get_studio, search_studios, explore_studios
+from .site.project import User, get_user
+
