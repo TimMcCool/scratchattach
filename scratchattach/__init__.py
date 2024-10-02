e2ba0af8726d642a6d8af6a829ba063075dac186
Tim <53166177+TimMcCool@users.noreply.github.com>
2024-10-02 02:48:40 +0200

    fixed circular import and other bug

--- scratchattach/__init__.py
+++ scratchattach/__init__.py
@@ -1,12 +1,11 @@
-from .cloud._base import BaseCloud
 from .cloud.cloud import ScratchCloud, TwCloud, get_cloud, get_scratch_cloud, get_tw_cloud
+from .cloud._base import BaseCloud
 
-from .eventhandlers._base import BaseEventHandler
 from .eventhandlers.cloud_server import init_cloud_server
+from .eventhandlers._base import BaseEventHandler
 
 from .other.other_apis import *
 
-from .site._base import BaseSiteComponent
 from .site.activity import Activity
 from .site.backpack_asset import BackpackAsset
 from .site.comment import Comment
@@ -14,6 +13,7 @@ from .site.cloud_activity import CloudActivity
 from .site.forum import ForumPost, ForumTopic, get_topic, get_topic_list
 from .site.project import Project, get_project, search_projects, explore_projects
 from .site.session import Session, login, login_by_id, login_by_session_string
-from .site.project import Studio, get_studio, search_studios, explore_studios
-from .site.project import User, get_user
+from .site.studio import Studio, get_studio, search_studios, explore_studios
+from .site.user import User, get_user
+from .site._base import BaseSiteComponent
 
