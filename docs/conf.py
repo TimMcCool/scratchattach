267810e4851b965c9b83dc07b385a28ab0ab0bf1
Tim <53166177+TimMcCool@users.noreply.github.com>
2024-10-20 16:22:18 +0200

    Update conf.py
    Signed-off-by: Tim <53166177+TimMcCool@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,8 @@ extensions = [
     'sphinx.ext.napoleon'
 ]
 
+# Automatically generate summary .rst files for modules
+autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,7 +46,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
