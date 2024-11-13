1a9e4d8b5e261f0b1678d573402ecca2a8d185be
FA ReTek <ldashunderscoreo@gmail.com>
2024-11-13 18:09:08 +0000

    added an enum i guess?

--- scratchattach/utils/supportedlangs.py
+++ scratchattach/utils/supportedlangs.py
@@ -2,23 +2,101 @@
 List of supported languages of scratch's translate and text2speech extensions.
 Adapted from https://translate-service.scratch.mit.edu/supported?language=en
 """
+from enum import Enum
 
 # Supported langs for translate
-SUPPORTED_LANGS = {'sq': 'Albanian', 'am': 'Amharic', 'ar': 'Arabic', 'hy': 'Armenian', 'az': 'Azerbaijani',
-                   'eu': 'Basque', 'be': 'Belarusian', 'bg': 'Bulgarian', 'ca': 'Catalan',
-                   'zh-tw': 'Chinese (Traditional)', 'hr': 'Croatian', 'cs': 'Czech', 'da': 'Danish', 'nl': 'Dutch',
-                   'en': 'English', 'eo': 'Esperanto', 'et': 'Estonian', 'fi': 'Finnish', 'fr': 'French',
-                   'gl': 'Galician', 'de': 'German', 'el': 'Greek', 'ht': 'Haitian Creole', 'hi': 'Hindi',
-                   'hu': 'Hungarian', 'is': 'Icelandic', 'id': 'Indonesian', 'ga': 'Irish', 'it': 'Italian',
-                   'ja': 'Japanese', 'kn': 'Kannada', 'ko': 'Korean', 'ku': 'Kurdish (Kurmanji)', 'la': 'Latin',
-                   'lv': 'Latvian', 'lt': 'Lithuanian', 'mk': 'Macedonian', 'ms': 'Malay', 'ml': 'Malayalam',
-                   'mt': 'Maltese', 'mi': 'Maori', 'mr': 'Marathi', 'mn': 'Mongolian', 'my': 'Myanmar (Burmese)',
-                   'fa': 'Persian', 'pl': 'Polish', 'pt': 'Portuguese', 'ro': 'Romanian', 'ru': 'Russian',
-                   'gd': 'Scots Gaelic', 'sr': 'Serbian', 'sk': 'Slovak', 'sl': 'Slovenian', 'es': 'Spanish',
-                   'sv': 'Swedish', 'te': 'Telugu', 'th': 'Thai', 'tr': 'Turkish', 'uk': 'Ukrainian', 'uz': 'Uzbek',
-                   'vi': 'Vietnamese', 'cy': 'Welsh', 'zu': 'Zulu', 'he': 'Hebrew', 'zh-cn': 'Chinese (Simplified)'}
-SUPPORTED_CODES = tuple(SUPPORTED_LANGS.keys())
-SUPPORTED_NAMES = tuple(SUPPORTED_LANGS.values())
+_TRANSLATE_SUPPORTED_LANGS = {'sq': 'Albanian', 'am': 'Amharic', 'ar': 'Arabic', 'hy': 'Armenian', 'az': 'Azerbaijani',
+                              'eu': 'Basque', 'be': 'Belarusian', 'bg': 'Bulgarian', 'ca': 'Catalan',
+                              'zh-tw': 'Chinese (Traditional)', 'hr': 'Croatian', 'cs': 'Czech', 'da': 'Danish',
+                              'nl': 'Dutch',
+                              'en': 'English', 'eo': 'Esperanto', 'et': 'Estonian', 'fi': 'Finnish', 'fr': 'French',
+                              'gl': 'Galician', 'de': 'German', 'el': 'Greek', 'ht': 'Haitian Creole', 'hi': 'Hindi',
+                              'hu': 'Hungarian', 'is': 'Icelandic', 'id': 'Indonesian', 'ga': 'Irish', 'it': 'Italian',
+                              'ja': 'Japanese', 'kn': 'Kannada', 'ko': 'Korean', 'ku': 'Kurdish (Kurmanji)',
+                              'la': 'Latin',
+                              'lv': 'Latvian', 'lt': 'Lithuanian', 'mk': 'Macedonian', 'ms': 'Malay', 'ml': 'Malayalam',
+                              'mt': 'Maltese', 'mi': 'Maori', 'mr': 'Marathi', 'mn': 'Mongolian',
+                              'my': 'Myanmar (Burmese)',
+                              'fa': 'Persian', 'pl': 'Polish', 'pt': 'Portuguese', 'ro': 'Romanian', 'ru': 'Russian',
+                              'gd': 'Scots Gaelic', 'sr': 'Serbian', 'sk': 'Slovak', 'sl': 'Slovenian', 'es': 'Spanish',
+                              'sv': 'Swedish', 'te': 'Telugu', 'th': 'Thai', 'tr': 'Turkish', 'uk': 'Ukrainian',
+                              'uz': 'Uzbek',
+                              'vi': 'Vietnamese', 'cy': 'Welsh', 'zu': 'Zulu', 'he': 'Hebrew',
+                              'zh-cn': 'Chinese (Simplified)'}
+
+TSL_CODES = tuple(_TRANSLATE_SUPPORTED_LANGS.keys())
+TSL_NAMES = tuple(_TRANSLATE_SUPPORTED_LANGS.values())
+
+
+class TranslateSupportedLangs(Enum):
+    Albanian = "sq"
+    Amharic = "am"
+    Arabic = "ar"
+    Armenian = "hy"
+    Azerbaijani = "az"
+    Basque = "eu"
+    Belarusian = "be"
+    Bulgarian = "bg"
+    Catalan = "ca"
+    Traditional_Chinese = "zh-tw"
+    Croatian = "hr"
+    Czech = "cs"
+    Danish = "da"
+    Dutch = "nl"
+    English = "en"
+    Esperanto = "eo"
+    Estonian = "et"
+    Finnish = "fi"
+    French = "fr"
+    Galician = "gl"
+    German = "de"
+    Greek = "el"
+    Haitian_Creole = "ht"
+    Hindi = "hi"
+    Hungarian = "hu"
+    Icelandic = "is"
+    Indonesian = "id"
+    Irish = "ga"
+    Italian = "it"
+    Japanese = "ja"
+    Kannada = "kn"
+    Korean = "ko"
+    Kurdish = "ku"
+    Kurmanji = "ku"
+    Latin = "la"
+    Latvian = "lv"
+    Lithuanian = "lt"
+    Macedonian = "mk"
+    Malay = "ms"
+    Malayalam = "ml"
+    Maltese = "mt"
+    Maori = "mi"
+    Marathi = "mr"
+    Mongolian = "mn"
+    Myanmar = "my"
+    Burmese = "my"
+    Persian = "fa"
+    Polish = "pl"
+    Portuguese = "pt"
+    Romanian = "ro"
+    Russian = "ru"
+    Scots_Gaelic = "gd"
+    Serbian = "sr"
+    Slovak = "sk"
+    Slovenian = "sl"
+    Spanish = "es"
+    Swedish = "sv"
+    Telugu = "te"
+    Thai = "th"
+    Turkish = "tr"
+    Ukrainian = "uk"
+    Uzbek = "uz"
+    Vietnamese = "vi"
+    Welsh = "cy"
+    Zulu = "zu"
+    Hebrew = "he"
+    Simplified_Chinese = "zh-cn"
+
 
 # Code for generating the dict again:
 # import requests
