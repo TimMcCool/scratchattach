9876baf62ce5a66e468b6b928a3bed8e96d01493
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2025-04-06 20:06:57 +0200

    Aktualisieren von setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '2.1.10'
+VERSION = '2.1.11'
 DESCRIPTION = 'A Scratch API Wrapper'
 LONG_DESCRIPTION = DESCRIPTION
 
