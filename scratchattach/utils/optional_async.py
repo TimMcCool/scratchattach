da59eea33bdf6f44a4da88409a646d4fe3044202
TheCommCraft <simon.c.gilde@gmail.com>
2025-08-03 22:26:40 +0200

    oa requests

--- scratchattach/utils/optional_async.py
+++ scratchattach/utils/optional_async.py
@@ -1,27 +1,31 @@
 from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from collections.abc import Awaitable, Generator, Callable
-from typing import Generic, TypeVar, ParamSpec
+from typing import Generic, TypeVar, ParamSpec, Optional, Union
+from functools import wraps
 import asyncio
 import time
+
+from . import requests
 
 P = ParamSpec("P")
 R = TypeVar("R")
 
 class CallableAwaitable(Generic[R], ABC, Awaitable[R]):
     result: R
-
+    
     @abstractmethod
     def sync_impl(self) -> R:
         pass
-
+    
     def __pos__(self) -> R:
         return self.sync_impl()
-
+    
     @abstractmethod
     async def async_impl(self) -> R:
         pass
-
+    
     def __await__(self) -> Generator[None, None, R]:
         return self.async_impl().__await__()
 
@@ -42,7 +46,7 @@ def optionally_async(func: Callable[P, Generator[CallableAwaitable, None, R]]) -
                             c.result = +c
                     except StopIteration as excp:
                         return excp.value
-
+                
                 async def async_impl(self) -> R:
                     i = func(*args, **kwargs)
                     try:
@@ -51,21 +55,100 @@ def optionally_async(func: Callable[P, Generator[CallableAwaitable, None, R]]) -
                             c.result = await c
                     except StopIteration as excp:
                         return excp.value
-
+            
             return Implementation()
     return Wrapped()
 
-class CASleep(CallableAwaitable[None]):
+def make_async(func: Callable[P, Generator[CallableAwaitable, None, R]]) -> Callable[P, Awaitable[R]]:
+    @wraps(func)
+    async def async_impl(*args: P.args, **kwargs: P.kwargs) -> R:
+        i = func(*args, **kwargs)
+        try:
+            while True:
+                c = next(i)
+                c.result = await c
+        except StopIteration as excp:
+            return excp.value
+    return async_impl
+
+def make_sync(func: Callable[P, Generator[CallableAwaitable, None, R]]) -> Callable[P, R]:
+    @wraps(func)
+    def sync_impl(*args: P.args, **kwargs: P.kwargs) -> R:
+        i = func(*args, **kwargs)
+        try:
+            while True:
+                c = next(i)
+                c.result = +c
+        except StopIteration as excp:
+            return excp.value
+    return sync_impl
+
+class CASleep(CallableAwaitable[bool]):
     amount: float
     
     def __init__(self, amount: float) -> None:
         self.amount = amount
     
     def sync_impl(self):
-        return time.sleep(self.amount)
+        time.sleep(self.amount)
+        return True
     
     async def async_impl(self):
-        return await asyncio.sleep(self.amount)
-    
+        await asyncio.sleep(self.amount)
+        return True
+
 def oa_sleep(amount: float):
     return CASleep(amount)
+
+class CARequest(CallableAwaitable[requests.AnyHTTPResponse]):
+    requests_session: requests.OAHTTPSession
+    method: requests.HTTPMethod
+    url: str
+    cookies: Optional[dict[str, str]]
+    headers: Optional[dict[str, str]]
+    params: Optional[dict[str, str]]
+    data: Optional[Union[dict[str, str], str]]
+    json: Optional[dict[str, str]]
+    
+    def __init__(
+        self,
+        requests_session: requests.OAHTTPSession,
+        method: requests.HTTPMethod,
+        url: str,
+        *,
+        cookies: Optional[dict[str, str]] = None,
+        headers: Optional[dict[str, str]] = None,
+        params: Optional[dict[str, str]] = None,
+        data: Optional[Union[dict[str, str], str]] = None,
+        json: Optional[dict[str, str]] = None
+    ) -> None:
+        self.requests_session = requests_session
+        self.method = method
+        self.url = url
+        self.cookies = cookies
+        self.headers = headers
+        self.params = params
+        self.data = data
+        self.json = json
+    
+    def sync_impl(self):
+        return self.requests_session.sync_request(
+            method = self.method,
+            url = self.url,
+            cookies = self.cookies,
+            headers = self.headers,
+            params = self.params,
+            data = self.data,
+            json = self.json
+        )
+    
+    async def async_impl(self):
+        return await self.requests_session.async_request(
+            method = self.method,
+            url = self.url,
+            cookies = self.cookies,
+            headers = self.headers,
+            params = self.params,
+            data = self.data,
+            json = self.json
+        )
--- scratchattach/utils/requests.py
+++ scratchattach/utils/requests.py
@@ -1,16 +1,209 @@
 from __future__ import annotations
 
 from collections.abc import MutableMapping, Iterator
-from typing import Optional
+from abc import ABC, abstractmethod
+from types import TracebackType
+from typing import Optional, Any, Self, Union
 from contextlib import contextmanager
+from enum import Enum, auto
+from dataclasses import dataclass, field
+import json
 
+from aiohttp.cookiejar import DummyCookieJar
 from typing_extensions import override
 from requests import Session as HTTPSession
 from requests import Response
+import aiohttp
 
 from . import exceptions
+from . import optional_async
 
 proxies: Optional[MutableMapping[str, str]] = None
+
+class HTTPMethod(Enum):
+    GET = auto()
+    POST = auto()
+    PUT = auto()
+    DELETE = auto()
+    HEAD = auto()
+    OPTIONS = auto()
+    PATCH = auto()
+    TRACE = auto()
+    @classmethod
+    def of(cls, name: str) -> HTTPMethod:
+        member_map = {
+            "GET": cls.GET,
+            "POST": cls.POST,
+            "PUT": cls.PUT,
+            "DELETE": cls.DELETE,
+            "HEAD": cls.HEAD,
+            "OPTIONS": cls.OPTIONS,
+            "PATCH": cls.PATCH,
+            "TRACE": cls.TRACE
+        }
+        return member_map[name]
+
+class AnyHTTPResponse(ABC):
+    request_method: HTTPMethod
+    status_code: int
+    content: bytes
+    text: str
+    headers: dict[str, str]
+    
+    def json(self) -> Any:
+        return json.loads(self.text)
+
+@dataclass
+class HTTPResponse(AnyHTTPResponse):
+    request_method: HTTPMethod = field(kw_only=True)
+    status_code: int = field(kw_only=True)
+    content: bytes = field(kw_only=True)
+    text: str = field(kw_only=True)
+    headers: dict[str, str] = field(kw_only=True)
+
+class OAHTTPSession(ABC):
+    error_handling: bool = True
+    @abstractmethod
+    def sync_request(
+        self,
+        method: HTTPMethod,
+        url: str,
+        *,
+        cookies: Optional[dict[str, str]] = None,
+        headers: Optional[dict[str, str]] = None,
+        params: Optional[dict[str, str]] = None,
+        data: Optional[Union[dict[str, str], str]] = None,
+        json: Optional[dict[str, str]] = None
+    ) -> AnyHTTPResponse:
+        pass
+    
+    @abstractmethod
+    async def async_request(
+        self,
+        method: HTTPMethod,
+        url: str,
+        *,
+        cookies: Optional[dict[str, str]] = None,
+        headers: Optional[dict[str, str]] = None,
+        params: Optional[dict[str, str]] = None,
+        data: Optional[Union[dict[str, str], str]] = None,
+        json: Optional[dict[str, str]] = None
+    ) -> AnyHTTPResponse:
+        pass
+
+    def check_response(self, r: AnyHTTPResponse):
+        if r.status_code == 403 or r.status_code == 401:
+            raise exceptions.Unauthorized(f"Request content: {r.content!r}")
+        if r.status_code == 500:
+            raise exceptions.APIError("Internal Scratch server error")
+        if r.status_code == 429:
+            raise exceptions.Response429("You are being rate-limited (or blocked) by Scratch")
+        if r.json() == {"code":"BadRequest","message":""}:
+            raise exceptions.BadRequest("Make sure all provided arguments are valid")
+    
+    
+    def _make_request(
+        self,
+        method: Union[HTTPMethod, str],
+        url: str,
+        *,
+        cookies: Optional[dict[str, str]] = None,
+        headers: Optional[dict[str, str]] = None,
+        params: Optional[dict[str, str]] = None,
+        data: Optional[Union[dict[str, str], str]] = None,
+        json: Optional[dict[str, str]] = None
+    ):
+        if isinstance(method, str):
+            _method = HTTPMethod.of(method.upper())
+            assert isinstance(_method, HTTPMethod)
+            method = _method
+        return optional_async.CARequest(
+            self,
+            method,
+            url,
+            cookies = cookies,
+            headers = headers,
+            params = params,
+            data = data,
+            json = json
+        )
+
+class SyncRequests(OAHTTPSession):
+    @override
+    def sync_request(self, method, url, *, cookies = None, headers = None, params = None, data = None, json = None):
+        try:
+            r = requests.request(
+                method.name,
+                url,
+                cookies = cookies,
+                headers = headers,
+                params = params,
+                data = data,
+                json = json,
+                proxies = proxies
+            )
+        except Exception as e:
+            raise exceptions.FetchError(e)
+        if self.error_handling:
+            self.check_response(r)
+        return HTTPResponse(
+            request_method=method,
+            status_code=r.status_code,
+            content=r.content,
+            text=r.text,
+            headers=r.headers
+        )
+    
+    async def async_request(self, method, url, *, cookies = None, headers = None, params = None, data = None, json = None):
+        raise NotImplementedError()
+
+class AsyncRequests(OAHTTPSession):
+    client_session: aiohttp.ClientSession
+    async def __aenter__(self) -> Self:
+        self.client_session = await aiohttp.ClientSession(cookie_jar=DummyCookieJar()).__aenter__()
+        return self
+    
+    async def __aexit__(
+        self,
+        exc_type: Optional[type[BaseException]] = None,
+        exc_val: Optional[BaseException] = None,
+        exc_tb: Optional[TracebackType] = None
+    ) -> None:
+        await self.client_session.__aexit__(exc_type, exc_val, exc_tb)
+    
+    @override
+    def sync_request(self, method, url, *, cookies = None, headers = None, params = None, data = None, json = None):
+        raise NotImplementedError()
+    
+    async def async_request(self, method, url, *, cookies = None, headers = None, params = None, data = None, json = None):
+        proxy = None
+        if url.startswith("http"):
+            proxy = proxies.get("http")
+        if url.startswith("https"):
+            proxy = proxies.get("https")
+        async with self.client_session.request(
+            method.name,
+            url,
+            cookies = cookies,
+            headers = headers,
+            params = params,
+            data = data,
+            json = json,
+            proxy = proxy
+        ) as resp:
+            assert isinstance(resp, aiohttp.ClientResponse)
+            content = await resp.read()
+            try:
+                text = content.decode(resp.get_encoding())
+            except Exception:
+                text = ""
+            return HTTPResponse(
+                request_method=method,
+                status_code=resp.status,
+                content=content,
+                text=text,
+                headers=resp.headers
+            )
 
 class Requests(HTTPSession):
     """
