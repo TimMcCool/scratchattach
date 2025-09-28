e83c63e5774723f3ada95b2cdad3297f05315356
Boss-1s <95505913+Boss-1s@users.noreply.github.com>
2025-09-28 03:46:46 -0500

    feat: Add note when encountering WebSocketBadStatusException (#490)
    Co-authored-by: retek <107722825+faretek1@users.noreply.github.com>
    Co-authored-by: TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
    Co-authored-by: faretek <ldashunderscoreo@gmail.com>

--- scratchattach/cloud/cloud.py
+++ scratchattach/cloud/cloud.py
@@ -8,6 +8,7 @@ from scratchattach.utils.requests import requests
 from scratchattach.utils import exceptions, commons
 from scratchattach.site import cloud_activity
 
+from websocket import WebSocketBadStatusException
 
 class ScratchCloud(BaseCloud):
     def __init__(self, *, project_id, _session=None):
@@ -26,7 +27,10 @@ class ScratchCloud(BaseCloud):
 
     def connect(self):
         self._assert_auth() # Connecting to Scratch's cloud websocket requires a login to the Scratch website
-        super().connect()
+        try:
+            super().connect()
+        except WebSocketBadStatusException as e:
+            raise WebSocketBadStatusException(f"Error: Scratch's Cloud system may be down. Please try again later.") from e
 
     def set_var(self, variable, value):
         self._assert_auth() # Setting a cloud var requires a login to the Scratch website
