7c005d5a55fe60c5a1cbfc4cb4e041101a6e8a49
faretek <ldashunderscoreo@gmail.com>
2025-10-05 13:06:48 +0100

    feat: make group for new user

--- scratchattach/cli/cmd/login.py
+++ scratchattach/cli/cmd/login.py
@@ -1,5 +1,6 @@
-from scratchattach.cli.context import ctx
+from scratchattach.cli.context import ctx, console
 from scratchattach.cli import db
+from rich.console import escape
 
 from getpass import getpass
 
@@ -21,9 +22,21 @@ def login():
 
         session = sa.login(username, password)
 
+    # register session
     db.conn.execute("BEGIN")
     db.cursor.execute(
         "INSERT OR REPLACE INTO SESSIONS (ID, USERNAME) "
         "VALUES (?, ?)", (session.id, session.username)
     )
     db.conn.commit()
+
+    # make new group
+    db.cursor.execute("SELECT NAME FROM GROUPS WHERE NAME = ?", (session.username,))
+    if not db.cursor.fetchone():
+        console.print(f"Registering [blue]{escape(session.username)}[/] as group")
+        db.conn.execute("BEGIN")
+        db.cursor.execute(
+            "INSERT INTO GROUPS (NAME, DESCRIPTION) "
+            "VALUES (?, ?)", (session.username, input(f"Description for {session.username}: "))
+        )
+        db.conn.commit()
