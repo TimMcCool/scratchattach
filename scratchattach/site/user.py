32553fd4f732d2dbe9e9d0ab915ce6a34d56040b
TheCommCraft <79996518+TheCommCraft@users.noreply.github.com>
2025-10-26 14:48:04 +0100

    fixed is_new_scratcher()

--- scratchattach/site/user.py
+++ scratchattach/site/user.py
@@ -7,6 +7,7 @@ import re
 import string
 from datetime import datetime, timezone
 from enum import Enum
+import warnings
 
 from typing_extensions import deprecated
 from bs4 import BeautifulSoup, Tag
@@ -198,10 +199,13 @@ class User(BaseSiteComponent):
             boolean : True if the user has the New Scratcher status, else False
         """
         try:
-            res = requests.get(f"https://scratch.mit.edu/users/{self.username}/").text
-            group=res[res.rindex('<span class="group">'):][:70]
-            return "new scratcher" in group.lower()
-        except Exception:
+            with requests.no_error_handling():
+                res = requests.get(f"https://scratch.mit.edu/users/{self.username}/").text
+                group = res[res.rindex('<span class="group">'):][:70]
+                return "new scratcher" in group.lower()
+
+        except Exception as e:
+            warnings.warn(f"Caught exception {e=}")
             return None
 
     def message_count(self):
