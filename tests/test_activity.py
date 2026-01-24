2fe58afb3803003a388ba0ac1c31989124a32230
faretek <ldashunderscoreo@gmail.com>
2026-01-24 08:44:16 +0000

    test: try removing sys.path
    this may cause tests to fail

--- tests/test_activity.py
+++ tests/test_activity.py
@@ -1,23 +1,22 @@
-import sys
 from datetime import datetime, timedelta, timezone
 import warnings
-
+import scratchattach as sa
+from scratchattach.utils import exceptions
+import util
 
 
 def test_activity():
-    sys.path.insert(0, ".")
-    import scratchattach as sa
-    from scratchattach.utils import exceptions
-    import util
     if not util.credentials_available():
-        warnings.warn("Skipped test_activity because there were no credentials available.")
+        warnings.warn(
+            "Skipped test_activity because there were no credentials available."
+        )
         return
     sess = util.session()
 
     # we cannot do assertions, but we can probe for any errors.
     messages = sess.messages()
     for msg in messages:
-        print(msg, end=' ')
+        print(msg, end=" ")
 
         try:
             target = msg.target()
