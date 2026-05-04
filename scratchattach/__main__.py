6f8e272e0de0cdc2489cda0c61fe8e606163e0bd
rtk <r@faretek.dev>
2026-05-04 16:16:00 +0100

    refactor: reduce complexity of __main__

--- scratchattach/__main__.py
+++ scratchattach/__main__.py
@@ -22,12 +22,12 @@ def main():
     )
 
     # Using walrus operator & ifs for artificial indentation
+    # TODO: there may be a better way to do this
     if commands := parser.add_subparsers(dest="command"):
         commands.add_parser("profile", help="View your profile")
         commands.add_parser("sessions", help="View session list")
         if login := commands.add_parser("login", help="Login to Scratch"):
-            login.add_argument("--sessid", dest="sessid", nargs="?", default=False, const=True,
-                               help="Login by session ID")
+            login.add_argument("--sessid", dest="sessid", nargs="?", default=False, const=True, help="Login by session ID")
         if group := commands.add_parser("group", help="View current session group"):
             if group_commands := group.add_subparsers(dest="group_command"):
                 group_commands.add_parser("list", help="List all session groups")
@@ -46,10 +46,13 @@ def main():
     parser.add_argument("-U", "--username", dest="username", help="Name of user to look at")
     parser.add_argument("-P", "--project", dest="project_id", help="ID of project to look at")
     parser.add_argument("-S", "--studio", dest="studio_id", help="ID of studio to look at")
-    parser.add_argument("-L", "--session_name", dest="session_name",
-                        help="Name of (registered) session/login to look at")
+    parser.add_argument("-L", "--session_name", dest="session_name", help="Name of (registered) session/login to look at")
 
     args = parser.parse_args(namespace=cli.ArgSpace())
+    handle_args(args, parser)
+
+
+def handle_args(args: cli.ArgSpace, parser: argparse.ArgumentParser):
     cli.ctx.args = args
     cli.ctx.parser = parser
 
@@ -63,31 +66,36 @@ def main():
         case "profile":
             cmd.profile()
         case None:
-            if args.username:
-                user = ctx.session.connect_user(args.username)
-                console.print(cli.try_get_img(user.icon, (30, 30)))
-                console.print(user)
-                return
-            if args.studio_id:
-                studio = ctx.session.connect_studio(args.studio_id)
-                console.print(cli.try_get_img(studio.thumbnail, (34, 20)))
-                console.print(studio)
-                return
-            if args.project_id:
-                project = ctx.session.connect_project(args.project_id)
-                console.print(cli.try_get_img(project.thumbnail, (30, 23)))
-                console.print(project)
-                return
-            if args.session_name:
-                if sess := ctx.db_get_sess(args.session_name):
-                    console.print(sess)
-                else:
-                    raise ValueError(f"No session logged in called {args.session_name!r} "
-                                     f"- try using `scratch sessions` to see available sessions")
-                return
-
-            parser.print_help()
+            handle_nocmd(args, parser)
 
 
-if __name__ == '__main__':
+def handle_nocmd(args: cli.ArgSpace, parser: argparse.ArgumentParser):
+    if args.username:
+        user = ctx.session.connect_user(args.username)
+        console.print(cli.try_get_img(user.icon, (30, 30)))
+        console.print(user)
+        return
+    if args.studio_id:
+        studio = ctx.session.connect_studio(args.studio_id)
+        console.print(cli.try_get_img(studio.thumbnail, (34, 20)))
+        console.print(studio)
+        return
+    if args.project_id:
+        project = ctx.session.connect_project(args.project_id)
+        console.print(cli.try_get_img(project.thumbnail, (30, 23)))
+        console.print(project)
+        return
+    if args.session_name:
+        if sess := ctx.db_get_sess(args.session_name):
+            console.print(sess)
+        else:
+            raise ValueError(
+                f"No session logged in called {args.session_name!r} - try using `scratch sessions` to see available sessions"
+            )
+        return
+
+    parser.print_help()
+
+
+if __name__ == "__main__":
     main()
