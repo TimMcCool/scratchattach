30cdecf6c70c81775a1b0f8c7d01f53c67460a7c
rtk <r@faretek.dev>
2026-05-04 19:31:31 +0100

    chore: remove placeholder
    see #493

--- scratchattach/__init__.py
+++ scratchattach/__init__.py
@@ -8,6 +8,7 @@ from .eventhandlers.cloud_storage import Database
 from .eventhandlers.combine import MultiEventHandler
 
 from .other.other_apis import *
+
 # from .other.project_json_capabilities import ProjectBody, get_empty_project_pb, get_pb_from_dict, read_sb3_file, download_asset
 from .utils.encoder import Encoding
 from .utils.enums import Languages, TTSVoices
@@ -17,7 +18,8 @@ from .utils.exceptions import (
     StudioAuthenticationWarning,
     ClassroomAuthenticationWarning,
     ProjectAuthenticationWarning,
-    UserAuthenticationWarning)
+    UserAuthenticationWarning,
+)
 
 from .site.activity import Activity, ActivityTypes
 from .site.backpack_asset import BackpackAsset
@@ -25,13 +27,11 @@ from .site.comment import Comment, CommentSource
 from .site.cloud_activity import CloudActivity
 from .site.forum import ForumPost, ForumTopic, get_topic, get_topic_list, youtube_link_to_scratch
 from .site.project import Project, get_project, search_projects, explore_projects
-from .site.session import Session, login, login_by_id, login_by_session_string, login_by_io, login_by_file, \
-    login_from_browser
+from .site.session import Session, login, login_by_id, login_by_session_string, login_by_io, login_by_file, login_from_browser
 from .site.studio import Studio, get_studio, search_studios, explore_studios
 from .site.classroom import Classroom, get_classroom
 from .site.user import User, get_user, Rank
 from .site._base import BaseSiteComponent
 from .site.browser_cookies import Browser, ANY, FIREFOX, CHROME, CHROMIUM, VIVALDI, EDGE, EDGE_DEV, SAFARI
-from .site.placeholder import PlaceholderProject, get_placeholder_project, create_placeholder_project
 
 from . import editor
