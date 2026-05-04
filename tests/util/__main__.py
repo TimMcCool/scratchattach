fc109fdbe1f0cc73a85cce5b904d5dd548acda28
rtk <r@faretek.dev>
2026-05-04 12:10:36 +0100

    fix: reduce complexity of tests/util/__main__:main

--- tests/util/__main__.py
+++ tests/util/__main__.py
@@ -16,27 +16,29 @@ def gen_keystr():
     return secrets.token_urlsafe(32)
 
 
-def main():
-    class Args(argparse.Namespace):
-        command: str
-        content: str
+class Args(argparse.Namespace):
+    command: str
+    content: str
 
+
+def main():
     parser = argparse.ArgumentParser()
 
+    # using if statements here for indentation to help organise subcommands/arguments
     if command := parser.add_subparsers(dest="command"):
         if encrypt := command.add_parser("e", help="Encrypt content"):
             encrypt.add_argument("content", nargs="?")
         if decrypt := command.add_parser("d", help="Decrypt content"):
             decrypt.add_argument("content", nargs="?")
-        if keygen := command.add_parser("keygen", help="Generate a key. You could set this to $FERNET_KEY if you want"):
-            ...
-        if vercelauth := command.add_parser("vercel", help="Output the vercel auth data."):
-            ...
-        if addmask := command.add_parser("addmask", help="Mask all secrets."):
-            ...
 
-    args = parser.parse_args(namespace=Args())
+        command.add_parser("keygen", help="Generate a key. You could set this to $FERNET_KEY if you want")
+        command.add_parser("vercel", help="Output the vercel auth data.")
+        command.add_parser("addmask", help="Mask all secrets.")
 
+    cmd(parser.parse_args(namespace=Args()))
+
+
+def cmd(args: Args):
     match args.command:
         case "e":
             if not args.content:
@@ -50,14 +52,13 @@ def main():
 
         case "keygen":
             print(gen_keystr())
-            
+
         case "vercel":
-            print(
-                "\n".join(vercel_auth())
-            )
+            print("\n".join(vercel_auth()))
 
         case "addmask":
             mask_all()
+
 
 if __name__ == "__main__":
     main()
