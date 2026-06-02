e7616c5edcb12d8cac1dd22f93df0129cd27358c
Boss_1s <95505913+Boss-1s@users.noreply.github.com>
2026-06-02 11:40:16 -0500

    chore: remove old test
    Signed-off-by: Boss_1s <95505913+Boss-1s@users.noreply.github.com>

--- tests/test_memberships.py
+++ tests/test_memberships.py
@@ -18,7 +18,7 @@ def test_memberships():
         sa.get_user("scratchattachv2"),
         sa.get_user("GoatyJules"),
         sa.get_user("SkittlesTheBunny"),
-        sa.get_user("Boss_1s") # not sure if it works since im banned
+        sa.get_user("Boss_1s")
     ]
 
     assert all(getattr(user, "is_member", False) for user in member_users)
@@ -29,33 +29,6 @@ def test_memberships():
 
     assert any(user.has_badge() for user in member_users)
     assert all(not user.has_badge() for user in nomember_users)
-
-    # keeping old test in case
-
-    #u1 = sa.get_user("-KittyMax-")
-    #assert u1.is_member  # NOTE: This may be wrong! I think it might be supposed to be False here...
-    #assert not u1.has_ears
-    #assert u1.has_badge()
-
-    #u2 = sa.get_user("scratchteam")
-    #assert u2.is_member
-    #assert u2.has_ears
-    #assert u2.has_badge()
-
-    #u3 = sa.get_user("scratchattachv2")
-    #assert not u3.is_member
-    #assert not u3.has_ears
-    #assert not u3.has_badge()
-
-    #u4 = sa.get_user("peekir")
-    #assert u4.is_member
-    #assert u4.has_ears
-    #assert u4.has_badge()
-
-    #u5 = sa.get_user("StardreamT2")
-    #assert u5.is_member
-    #assert u5.has_ears
-    #assert not u5.has_badge()
 
 
 if __name__ == "__main__":
